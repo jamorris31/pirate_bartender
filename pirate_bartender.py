67cee961ca80a9fa3b1aa8b1a0847f0dcb9df899
jamorris31 <jamorris31@gmail.com>
2015-03-10 22:19:04 +0000

    Initial commit

---
+++ pirate_bartender.py
@@ -0,0 +1,62 @@
+import random
+
+def askQuestion():
+  questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?"
+  }
+
+  answers = {}
+
+  for key in questions.keys():
+    my_input = raw_input(questions[key])
+    if my_input == "yes" or my_input == "y":
+      answers[key] = True
+    else:
+      answers[key] = False
+  return answers
+
+def makeDrink(theAnswers):
+  ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+  }
+  theDrink = []
+  for key in theAnswers:
+    if theAnswers[key] == True:
+      theDrink.append(random.choice(ingredients[key]))
+  return theDrink
+
+    
+if __name__ == '__main__':
+  print makeDrink(askQuestion())
+
+#print answers
+#print questions["strong"]
+#if len(sys.argv) == 1:
+#   my_input = raw_input("Enter something!")
+#else: 
+ #  my_input = sys.argv[1]
+    
+#if raw_input == "yes" or "y": 
+#  answers["strong"] = true
+#else:
+ # answers["strong"] = false
+
+  
+#questions["strong"]
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+}
+
