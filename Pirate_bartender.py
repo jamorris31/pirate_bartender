6f682feb6f2e20643e71c9039939738e6e5933f6
jamorris31 <jamorris31@gmail.com>
2015-03-23 19:05:29 +0000

    new version

--- Pirate_bartender.py
+++ Pirate_bartender.py
@@ -1,39 +1,5 @@
 import random
 
-questions = {
-    "strong": "Do ye like yer drinks strong?",
-    "salty": "Do ye like it with a salty tang?",
-    "bitter": "Are ye a lubber who likes it bitter?",
-    "sweet": "Would ye like a bit of sweetness with yer poison?",
-    "fruity": "Are ye one for a fruity finish?"
-  }
-
-ingredients = {
-    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
-    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
-    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
-    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
-    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
-  }
-
-def askQuestion():
-  answers = {}
-  for key in questions.keys():
-    my_input = raw_input(questions[key])
-    if my_input == "yes" or my_input == "y":
-      answers[key] = True
-    else:
-      answers[key] = False
-  return answers
-
-
-def makeDrink(theAnswers):
-  theDrink = []
-  for key in theAnswers:
-    if theAnswers[key] == True:
-      theDrink.append(random.choice(ingredients[key]))
-import random
-
 def askquestion():
     questions = {
         "strong": "Do ye like yer drinks strong? ",
@@ -99,4 +65,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-                   
